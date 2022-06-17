1ae678d986553a65443d36098fa32fde2f1d8b76
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-17 20:29:23 -0300

    Update IPermissionsRepository.ts

--- src/modules/account/users/repositories/IPermissionsRepository.ts
+++ src/modules/account/users/repositories/IPermissionsRepository.ts
@@ -4,10 +4,9 @@ import { Permission } from "../infra/typeorm/entities/permission"
 interface IPermissionsRepository {
     create(data: ICreatePermissionDTO): Promise<Permission>;
     findByName(name: string): Promise<Permission>;
-    findById(id: string): Promise<Permission>;
+    findById(id: number): Promise<Permission>;
     list(): Promise<Permission[]>;
-    delete(id: string): Promise<void>;
-    
+    delete(id: number): Promise<void>;
 }
 
 export { IPermissionsRepository }
