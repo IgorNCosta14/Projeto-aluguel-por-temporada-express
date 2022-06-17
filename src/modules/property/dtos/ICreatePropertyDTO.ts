ae8e448fd7e13be9592d7557b91f63a055a69422
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-17 20:42:10 -0300

    added propertyOwner and updatedAt to property DTO

--- src/modules/property/dtos/ICreatePropertyDTO.ts
+++ src/modules/property/dtos/ICreatePropertyDTO.ts
@@ -2,11 +2,13 @@ interface ICreatePropertyDTO {
     id?: string;
     propertyName: string;
     description: string;
+    propertyOwner?: string;
     zipCode: string;
     typeProperty: string;
     available?: boolean;
     dailyRate: number;
     createdAt?: Date;
+    updatedAt?: Date;
   }
   
   export { ICreatePropertyDTO }
