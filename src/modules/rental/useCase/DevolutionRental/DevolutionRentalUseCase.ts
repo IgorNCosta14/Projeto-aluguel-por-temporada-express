2522f7936f80e76d591da21cc03d471be938d2d0
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-23 20:09:10 -0300

    Added rentalTotalRate to useCase

--- src/modules/rental/useCase/DevolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rental/useCase/DevolutionRental/DevolutionRentalUseCase.ts
@@ -1,7 +1,8 @@
 import { IPropertiesRepository } from "@modules/property/repositories/IPropertiesRepository";
 import { Rental } from "@modules/rental/infra/typeorm/entities/rental";
 import { IRentalsRepository } from "@modules/rental/repositories/IRentalsRepository";
-import { IDateProvider } from "@shared/container/providers/DateProvider/IdateProvider";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
 
 @injectable()
@@ -20,17 +21,24 @@ class DevolutionRentalUseCase {
     async execute(rentalId: string): Promise<Rental> {
         const rental = await this.rentalsRepository.findById(rentalId);
 
+        if(rental.endDate != null || rental.totalRate != null) {
+            throw new AppError("Rent already finished!")
+        }
+
         const property = await this.propertiesRepository.findById(rental.propertyId);
 
-        rental.endDate = rental.expectedReturnDate;
-        const rentalTotalRate = (this.dateProvider.compare(rental.endDate, rental.startDate)*property.dailyRate);
+        rental.endDate = this.dateProvider.dateNow();
+
+        const dateDifference = this.dateProvider.compare(rental.expectedReturnDate, rental.startDate)
+
+        const rentalTotalRate = (Math.ceil(dateDifference)*property.dailyRate);
 
         rental.totalRate = rentalTotalRate;
 
         await this.rentalsRepository.create(rental);
 
         const available = true;
-        const id = rental.propertyId
+        const id = rental.propertyId;
 
         await this.propertiesRepository.updateAvailableState(id, available);
 
