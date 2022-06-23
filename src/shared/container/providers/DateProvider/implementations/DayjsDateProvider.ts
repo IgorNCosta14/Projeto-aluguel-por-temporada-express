3f0424865e1eb2c0562729e2fd96ad575ae19293
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-23 20:10:02 -0300

    Fixed methods to calculate date difference

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -1,11 +1,10 @@
-import { IDateProvider } from "../IdateProvider";
+import { IDateProvider } from "../IDateProvider";
 import utc from "dayjs/plugin/utc"
 import dayjs from "dayjs";
 
 dayjs.extend(utc)
 
 class DateProvider implements IDateProvider{
-
     convertToUTC(date: Date): string {
         return dayjs(date).utc().local().format();
       }
@@ -18,13 +17,8 @@ class DateProvider implements IDateProvider{
         const endDateInUTC = this.convertToUTC(endDate)
         const startDateInUTC = this.convertToUTC(startDate)
 
-        return dayjs(startDateInUTC).diff(endDateInUTC, "day");
+        return dayjs(startDateInUTC).diff(endDateInUTC, "day", true);
     }
-
-    addDays(days: number): Date {
-        return dayjs().add(days, "days").toDate();
-    }
-
 }
 
 export { DateProvider }
