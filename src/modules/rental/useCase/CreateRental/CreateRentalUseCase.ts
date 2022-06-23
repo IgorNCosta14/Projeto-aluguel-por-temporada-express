270672c8c0545c4955321076757a007ebd2934ab
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-23 20:10:27 -0300

    Added expectedTotalRate

--- src/modules/rental/useCase/CreateRental/CreateRentalUseCase.ts
+++ src/modules/rental/useCase/CreateRental/CreateRentalUseCase.ts
@@ -2,6 +2,7 @@ import { IPropertiesRepository } from "@modules/property/repositories/IPropertie
 import { ICreateRentalDTO } from "@modules/rental/dtos/ICreateRentalDTO";
 import { Rental } from "@modules/rental/infra/typeorm/entities/rental";
 import { IRentalsRepository } from "@modules/rental/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
 
@@ -14,17 +15,29 @@ class CreateRentalUseCase {
 
         @inject("PropertiesRepository")
         private propertiesRepository: IPropertiesRepository,
+
+        @inject("DateProvider")
+        private dateProvider: IDateProvider
     ){}
 
     async execute({ propertyId, userId, expectedReturnDate}: ICreateRentalDTO): Promise<Rental> {
         
-        const propertyNotAvailable = await this.propertiesRepository.findById(propertyId);
+        const findProperty = await this.propertiesRepository.findById(propertyId);
 
-        if(propertyNotAvailable.available === false) {
+        if(findProperty.available === false) {
             throw new AppError("Property not available!")
         }
 
-        const rental = await this.rentalsRepository.create({ propertyId, userId, expectedReturnDate});
+        const startDate = this.dateProvider.dateNow()
+        const endDate = expectedReturnDate
+
+        const dateDifference = this.dateProvider.compare(endDate, startDate)
+
+        const expectedTotalRate = ((Math.ceil(dateDifference))*findProperty.dailyRate);
+
+        console.log(Math.ceil(dateDifference), startDate, endDate, expectedTotalRate)
+
+        const rental = await this.rentalsRepository.create({ propertyId, userId, expectedReturnDate, expectedTotalRate });
 
         const available = false;
         const id = propertyId
