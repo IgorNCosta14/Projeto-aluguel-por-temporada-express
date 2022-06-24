e9481d76f926d49c85bb421cd104ce22f8173d27
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-24 20:07:29 -0300

    Created rental new routes

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,7 +1,11 @@
 import { CreateRentalController } from "@modules/rental/useCase/CreateRental/CreateRentalController";
+import { DeleteRentalController } from "@modules/rental/useCase/DeleteRental/DeleteRentalController";
 import { DevolutionRentalController } from "@modules/rental/useCase/DevolutionRental/DevolutionRentalController";
-import { FindRentalByUserIdController } from "@modules/rental/useCase/FindByUserIdRental/FindRentalByUserIdController";
+import { FindRentalByUserIdController } from "@modules/rental/useCase/FindRentalByUserId/FindRentalByUserIdController";
+import { ListFinishedRentalsControllers } from "@modules/rental/useCase/ListFinishedRentals/ListFinishedRentalsControllers";
 import { ListRentalController } from "@modules/rental/useCase/ListRentals/ListRentalController";
+import { ListRentalsInProgressControllers } from "@modules/rental/useCase/ListRentalsInProgress/ListRentalsInProgressControllers";
+import { UpdateRentalController } from "@modules/rental/useCase/UpdateRental/UpdateRentalController";
 import { Router } from "express";
 import { checkAdmin } from "../middlewares/checkAdmin";
 import { checkAuthenticated } from "../middlewares/checkAuthenticates";
@@ -13,10 +17,20 @@ const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
 const findRentalByUserIdController = new FindRentalByUserIdController();
 const listRentalController = new ListRentalController();
+const listRentalsInProgressControllers = new ListRentalsInProgressControllers();
+const listFinishedRentalsControllers = new ListFinishedRentalsControllers();
+const updateRentalController = new UpdateRentalController();
+const deleteRentalController = new DeleteRentalController();
+
 
 rentalsRoutes.post("/", checkAuthenticated, createRentalController.handle);
 rentalsRoutes.get("/userrental", checkAuthenticated, findRentalByUserIdController.handle);
 rentalsRoutes.get("/", checkAuthenticated, checkAdmin, listRentalController.handle);
-rentalsRoutes.patch("/devolution/:id", checkAuthenticated, checkLandLord, devolutionRentalController.handle)
+rentalsRoutes.get("/inprogress", checkAuthenticated, checkAdmin, listRentalsInProgressControllers.handle);
+rentalsRoutes.get("/finished", checkAuthenticated, checkAdmin, listFinishedRentalsControllers.handle);
+rentalsRoutes.patch("/devolution/:id", checkAuthenticated, checkLandLord, devolutionRentalController.handle);
+rentalsRoutes.patch("/update/:id", checkAuthenticated, checkAdmin, updateRentalController.handle)
+rentalsRoutes.delete("/delete/:id", checkAuthenticated, checkAdmin, deleteRentalController.handle)
+
 
 export { rentalsRoutes };
