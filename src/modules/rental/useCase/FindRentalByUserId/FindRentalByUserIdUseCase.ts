8a971da4f4e685f701cc01237070cdee6bea8e16
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-06-24 20:09:45 -0300

    Created find user rentals by id usecase and controller

---
+++ src/modules/rental/useCase/FindRentalByUserId/FindRentalByUserIdUseCase.ts
@@ -0,0 +1,24 @@
+import { Rental } from "@modules/rental/infra/typeorm/entities/rental";
+import { IRentalsRepository } from "@modules/rental/repositories/IRentalsRepository";
+import { AppError } from "@shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+class FindRentalByUserIdUseCase {
+    constructor(
+        @inject("RentalsRepository")
+        private rentalsRepository: IRentalsRepository
+    ){}
+
+    async execute(id: string): Promise<Rental[]> {
+        const rental = await this.rentalsRepository.findRentalByUserId(id);
+
+        if(!rental) {
+            throw new AppError("Rental not found!")
+        }
+
+        return rental;
+    }
+}
+
+export { FindRentalByUserIdUseCase }
