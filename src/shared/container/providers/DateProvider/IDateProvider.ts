f1c5eccc0564780f02ce7032a02191044d1baf4b
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-07-11 21:42:13 -0300

    Update IDateProvider.ts

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
     convertToUTC(date: Date): string;
     dateNow(): Date;
     compare(startDate: Date, endDate: Date): number;
+    addDays(days: number): Date;
 }
 
 export { IDateProvider }
